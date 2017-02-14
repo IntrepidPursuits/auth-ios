8d640c8285831c0a98ff40a60d2d23e4e5679aa1
Mark Daigneault <markd@intrepid.io>
2017-02-14 09:56:33 -0500

    Initial Commit

---
+++ AuthKit/AuthKit.h
@@ -0,0 +1,19 @@
+//
+//  AuthKit.h
+//  AuthKit
+//
+//  Created by Mark Daigneault on 2/14/17.
+//  Copyright © 2017 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuthKit.
+FOUNDATION_EXPORT double AuthKitVersionNumber;
+
+//! Project version string for AuthKit.
+FOUNDATION_EXPORT const unsigned char AuthKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthKit/PublicHeader.h>
+
+
